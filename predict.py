d9298e2f665b6a4608e6be9b6dc0f417f2727e90
Chigozie Nri <chigozie@replicate.com>
2023-11-12 10:28:48 +0000

    SDXL

--- predict.py
+++ predict.py
@@ -6,117 +6,117 @@ import os
 import sys
 sys.path.extend(['/AnimateDiff'])
 import tempfile
-import diffusers
-from diffusers import AutoencoderKL, DDIMScheduler
+from diffusers import AutoencoderKL, EulerDiscreteScheduler
 from omegaconf import OmegaConf
-from safetensors import safe_open
 import torch
-from tqdm.auto import tqdm
-from transformers import CLIPTextModel, CLIPTokenizer
+from transformers import CLIPTextModel, CLIPTokenizer, CLIPTextModelWithProjection
 from animatediff.models.unet import UNet3DConditionModel
 from animatediff.pipelines.pipeline_animation import AnimationPipeline
-from animatediff.utils.util import save_videos_grid
-from animatediff.utils.convert_from_ckpt import convert_ldm_unet_checkpoint, convert_ldm_vae_checkpoint
-from animatediff.utils.convert_lora_safetensor_to_diffusers import convert_lora
-
+from animatediff.utils.util import save_videos_grid, load_weights
 from diffusers.utils.import_utils import is_xformers_available
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
-        pretrained_model_path = "/AnimateDiff/models/StableDiffusion/stable-diffusion-v1-5"
+        pretrained_model_path = "/AnimateDiff/models/StableDiffusion"
         self.tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_path, subfolder="tokenizer")
         self.text_encoder = CLIPTextModel.from_pretrained(pretrained_model_path, subfolder="text_encoder")
         self.vae = AutoencoderKL.from_pretrained(pretrained_model_path, subfolder="vae")
+        self.tokenizer_two = CLIPTokenizer.from_pretrained(pretrained_model_path, subfolder="tokenizer_2")
+        self.text_encoder_two = CLIPTextModelWithProjection.from_pretrained(pretrained_model_path, subfolder="text_encoder_2")
 
     def predict(
         self,
         motion_module: str = Input(
             description="Select a Motion Model",
-            default="mm_sd_v14",
+            default="mm_sdxl_v10_beta",
             choices=[
-                "mm_sd_v14",
-                "mm_sd_v15",
-                "mm_sd_v15_v2"
+                "mm_sdxl_v10_beta"
             ],
         ),
-        path: str = Input(
-            default="toonyou_beta3.safetensors",
+        checkpoint: str = Input(
+            default="dynavision",
             choices=[
-                "toonyou_beta3.safetensors",
-                "lyriel_v16.safetensors",
-                "rcnzCartoon3d_v10.safetensors",
-                "majicmixRealistic_v5Preview.safetensors",
-                "realisticVisionV40_v20Novae.safetensors"
+                "dynavision",
+                "dreamshaper",
+                "deepblue"
             ],
-            description="Select a Module",
+            description="Select a checkpoint",
         ),
-        prompt: str = Input(description="Input prompt", default="masterpiece, best quality, 1girl, solo, cherry blossoms, hanami, pink flower, white flower, spring season, wisteria, petals, flower, plum blossoms, outdoors, falling petals, white hair, black eyes"),
+        use_checkpoint: bool = Input(default=False),
+        aspect: str = Input(
+            default="1:1",
+            choices=[
+                "9:16",
+                "2:3",
+                "1:1",
+                "3:2",
+                "16:9",
+            ]
+        ),
+        video_length: int = Input(description="video length", ge=16, default=16),
+        prompt: str = Input(description="Input prompt", default="A panda standing on a surfboard in the ocean in sunset, 4k, high resolution.Realistic, Cinematic, high resolution"),
         n_prompt: str = Input(description="Negative prompt", default=""),
         steps: int = Input(description="Number of inference steps", ge=1, le=100, default=25),
-        guidance_scale: float = Input(description="guidance scale", ge=1, le=10, default=7.5),
+        guidance_scale: float = Input(description="guidance scale", ge=1, le=10, default=8.5),
         seed: int = Input(description="Seed (0 = random, maximum: 2147483647)", default=None),
     ) -> Path:
         """Run a single prediction on the model"""
-        lora_alpha=0.8
         base=""
+
+        aspect_to_width_height = {
+            "9:16": (768,1344),
+            "2:3": (832,1216),
+            "1:1": (1024,1024),
+            "3:2": (1216,832),
+            "16:9": (1344,768),
+        }
+        (width, height) = aspect_to_width_height[aspect]
         # Create paths and load motion model
-        newPath = "models/DreamBooth_LoRA/"+path
+        newPath = f"/AnimateDiff/models/DreamBooth_LoRA/{checkpoint}.safetensors"
         motion_path = "/AnimateDiff/models/Motion_Module/"+motion_module+".ckpt"
-        # Support new v2 motion module
-        if motion_module.endswith("v2"):
-            inference_config_file = "/AnimateDiff/configs/inference/inference-v2.yaml"
-        else:
-            inference_config_file = "/AnimateDiff/configs/inference/inference-v1.yaml"
+        inference_config_file = "/AnimateDiff/configs/inference/inference.yaml"
         # Load configuration
         inference_config = OmegaConf.load(inference_config_file)
+
         self.unet = UNet3DConditionModel.from_pretrained_2d(
-            "/AnimateDiff/models/StableDiffusion/stable-diffusion-v1-5",
+            "/AnimateDiff/models/StableDiffusion",
             subfolder="unet",
             unet_additional_kwargs=OmegaConf.to_container(inference_config.unet_additional_kwargs)
         )
+        scheduler = EulerDiscreteScheduler(timestep_spacing='leading', steps_offset=1,**OmegaConf.to_container(inference_config.noise_scheduler_kwargs))
         self.pipeline = AnimationPipeline(
             vae=self.vae, text_encoder=self.text_encoder, tokenizer=self.tokenizer, unet=self.unet,
-            scheduler=DDIMScheduler(**OmegaConf.to_container(inference_config.noise_scheduler_kwargs)),
+            scheduler=scheduler,
+            text_encoder_2 = self.text_encoder_two, tokenizer_2=self.tokenizer_two
         ).to("cuda")
-        if is_xformers_available(): self.unet.enable_xformers_memory_efficient_attention()
+        if is_xformers_available():
+            self.unet.enable_xformers_memory_efficient_attention()
+        
+        # TODO: Check for LoRA
+        # Load model weights
+        if use_checkpoint:
+            self.pipeline = load_weights(
+                pipeline = self.pipeline,
+                motion_module_path = motion_path,
+                ckpt_path = newPath,
+                lora_path = "",
+                lora_alpha = 0.8
+            )
+        else:
+            self.pipeline = load_weights(
+                pipeline = self.pipeline,
+                motion_module_path = motion_path,
+                ckpt_path = "",
+                lora_path = "",
+                lora_alpha = 0.8
+            )
 
-        motion_module_state_dict = torch.load(motion_path, map_location="cpu")
-        missing, unexpected = self.unet.load_state_dict(motion_module_state_dict, strict=False)
-        assert len(unexpected) == 0
-
-        if path != "":
-            fullPath = "/AnimateDiff/"+newPath
-            if path.endswith(".ckpt"):
-                state_dict = torch.load(fullPath)
-                self.unet.load_state_dict(state_dict)
-
-            elif path.endswith(".safetensors"):
-                state_dict = {}
-                base_state_dict = {}
-                with safe_open(fullPath, framework="pt", device="cpu") as f:
-                    for key in f.keys():
-                        state_dict[key] = f.get_tensor(key)
-
-                is_lora = all("lora" in k for k in state_dict.keys())
-                if not is_lora:
-                    base_state_dict = state_dict
-                else:
-                    base_state_dict = {}
-                    with safe_open(base, framework="pt", device="cpu") as f:
-                        for key in f.keys():
-                            base_state_dict[key] = f.get_tensor(key)
-                # vae
-                converted_vae_checkpoint = convert_ldm_vae_checkpoint(base_state_dict, self.vae.config)
-                self.vae.load_state_dict(converted_vae_checkpoint)
-                # unet
-                converted_unet_checkpoint = convert_ldm_unet_checkpoint(base_state_dict, self.unet.config)
-                self.unet.load_state_dict(converted_unet_checkpoint, strict=False)
-
-                if is_lora:
-                    self.pipeline = convert_lora(self.pipeline, state_dict, alpha=lora_alpha)
-
-        self.pipeline.to("cuda")
+        self.pipeline.unet = self.pipeline.unet.half()
+        self.pipeline.text_encoder = self.pipeline.text_encoder.half()
+        self.pipeline.text_encoder_2 = self.pipeline.text_encoder_2.half()
+        self.pipeline.enable_model_cpu_offload()
+        self.pipeline.enable_vae_slicing()
 
         if seed is None:
             seed = int.from_bytes(os.urandom(4), "big")
@@ -133,9 +133,9 @@ class Predictor(BasePredictor):
             negative_prompt     = n_prompt,
             num_inference_steps = steps,
             guidance_scale      = guidance_scale,
-            width               = 512,
-            height              = 512,
-            video_length        = 16,
+            width               = width,
+            height              = height,
+            single_model_length = video_length,
         ).videos
 
         samples = torch.concat([sample])
